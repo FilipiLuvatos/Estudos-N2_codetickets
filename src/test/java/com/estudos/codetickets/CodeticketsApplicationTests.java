a392b14f3f6806509297f7422f04aafda8af5092
FilipiDeLuca <filipivalim.NTTD@latam.com>
2025-05-06 12:03:03 -0300

    Preparando ambiente

---
+++ src/test/java/com/estudos/codetickets/CodeticketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.codetickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeticketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
